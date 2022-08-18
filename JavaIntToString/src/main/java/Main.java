aae0390b79a0da93b40f5866f957da97a11e1621
KamilChabros <kamilchabros@wp.pl>
2022-08-18 20:08:59 +0200

    Some changes in files; JavaIntToString converter done;

---
+++ JavaIntToString/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String s = String.valueOf(n);
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer.");
+        }
+    }
+}

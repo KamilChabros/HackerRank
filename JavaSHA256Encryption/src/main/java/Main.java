27d61d6b521ada8fba867c368a45e95ed9fa6354
KamilChabros <kamilchabros@wp.pl>
2022-09-05 19:25:13 +0200

    JavaSHA256EncryptionPrint exercise done;

--- JavaSHA256Encryption/src/main/java/Main.java
+++ JavaSHA256Encryption/src/main/java/Main.java
@@ -5,19 +5,21 @@ import jakarta.xml.bind.DatatypeConverter;
 
 public class Main {
     public static void main(String[] args) throws NoSuchAlgorithmException {
+
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
         scanner.close();
 
-//        MessageDigest provides functionality of a message digest algorythm (SHA-1, SHA-256, MD5)
-        MessageDigest md = MessageDigest.getInstance("MD5");
-
+        //        MessageDigest provides functionality of a message digest algorythm (SHA-1, SHA-256, MD5)
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(s.getBytes());
-//        digest() method is called to calculate message digest
-//        of an input, digest() return array of byte
+
         byte[] digest = md.digest();
-        String myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+        //        digest() method is called to calculate message digest
+        //        of an input, digest() return array of byte
+        String myHash = DatatypeConverter.printHexBinary(digest).toLowerCase();
 
         System.out.println(myHash);
+
     }
 }

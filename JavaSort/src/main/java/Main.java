a9afa83b40258406efa3018d552eced83bc9ae0b
KamilChabros <kamilchabros@wp.pl>
2022-09-21 19:38:55 +0200

    JavaSort exercise done;

---
+++ JavaSort/src/main/java/Main.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class Checker implements Comparator<Student> {
+
+    @Override
+    public int compare(Student a, Student b) {
+        if (a.getCgpa() == b.getCgpa()) {
+            return a.getFirstName() != b.getFirstName() ? a.getFirstName().compareTo(b.getFirstName()) :
+                    b.getId() - a.getId();
+        } else {
+//            swapping b and a, changing order to increasing
+//            below result shows decreasing order
+            return (int) (1000 * (b.getCgpa() - a.getCgpa()));
+        }
+    }
+}
+
+class Student {
+    private int id;
+    private String firstName;
+    private double cgpa;
+
+    public Student(int id, String firstName, double cgpa) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testcases = Integer.parseInt(scanner.nextLine());
+//        int testcases = scanner.nextInt();
+        List<Student> studentList = new ArrayList<>();
+        while (testcases > 0) {
+            int id = scanner.nextInt();
+            String firstName = scanner.next();
+            double cgpa = scanner.nextDouble();
+
+            Student student = new Student(id, firstName, cgpa);
+            studentList.add(student);
+            testcases--;
+        }
+        Collections.sort(studentList, new Checker());
+        for (Student student: studentList){
+            System.out.println(student.getFirstName());
+        }
+    }
+}
